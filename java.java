ec231201f5ebfed4a327117027193d2d7cb0d769
S H Mahammed Saleeth <selith514@gmail.com>
2026-01-24 20:38:42 +0530

    Add EvenOdd class for number parity check
    Added EvenOdd class to check if a number is even or odd.

--- java.java
+++ java.java
@@ -13,4 +13,24 @@ class SumTwoNumbers {
         int sum = a + b;
         System.out.println("Sum = " + sum);
     }
+
+}
+
+\\ Day -2 Pratice \\
+    
+import java.util.Scanner;
+
+class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
 }

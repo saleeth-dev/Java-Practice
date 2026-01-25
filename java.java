6d427a48439f0d698830f2d5271bed4891867259
S H Mahammed Saleeth <selith514@gmail.com>
2026-01-25 19:43:51 +0530

    Add MiniCalculator class for basic arithmetic operations

--- java.java
+++ java.java
@@ -34,3 +34,31 @@ class EvenOdd {
         }
     }
 }
+
+\\ Day - 3 Practice \\
+
+import java.util.Scanner;
+
+class MiniCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+
+        if (b != 0) {
+            System.out.println("Division = " + (a / b));
+        } else {
+            System.out.println("Division not possible (divide by zero)");
+        }
+    }
+}
+
+

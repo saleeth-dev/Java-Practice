0da48bde34b930caccf43669cf8dfc92752559e4
S H Mahammed Saleeth <selith514@gmail.com>
2026-01-27 16:00:13 +0530

    Add Bank Management System mini project

--- java.java
+++ java.java
@@ -89,6 +89,93 @@ class NumberGuessGame {
         } while (guess != number);
     }
 }
+//Day 5 – Bank Management System mini project//
+
+import java.util.Scanner;
+
+class BankManagementSystem {
+
+    static Scanner sc = new Scanner(System.in);
+    static String name;
+    static int accountNumber;
+    static double balance = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Welcome to Simple Bank Management System =====");
+
+        createAccount();
+
+        int choice;
+        do {
+            showMenu();
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    deposit();
+                    break;
+                case 2:
+                    withdraw();
+                    break;
+                case 3:
+                    checkBalance();
+                    break;
+                case 4:
+                    System.out.println("Thank you for using our bank system!");
+                    break;
+                default:
+                    System.out.println("Invalid choice! Try again.");
+            }
+        } while (choice != 4);
+    }
+
+    static void createAccount() {
+        System.out.print("Enter your name: ");
+        name = sc.next();
+
+        System.out.print("Enter account number: ");
+        accountNumber = sc.nextInt();
+
+        System.out.println("Account created successfully!");
+    }
+
+    static void showMenu() {
+        System.out.println("\n------ MENU ------");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Check Balance");
+        System.out.println("4. Exit");
+    }
+
+    static void deposit() {
+        System.out.print("Enter amount to deposit: ");
+        double amount = sc.nextDouble();
+        balance += amount;
+        System.out.println("Deposit successful!");
+    }
+
+    static void withdraw() {
+        System.out.print("Enter amount to withdraw: ");
+        double amount = sc.nextDouble();
+
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdraw successful!");
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    static void checkBalance() {
+        System.out.println("Account holder: " + name);
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("Current balance: ₹" + balance);
+    }
+}
+
+
 
 
 

fc07a2d268af47dae1746520562d614b73ee39f5
S H Mahammed Saleeth <selith514@gmail.com>
2026-02-11 22:22:25 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- java.java
+++ java.java
@@ -981,6 +981,27 @@ function App() {
 
 export default App;
 
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
 
 
 

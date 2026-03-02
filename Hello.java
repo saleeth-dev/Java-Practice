5cecddc755bca5110dde3a30d76f60c1a5b7cfdf
S H Mahammed Saleeth <selith514@gmail.com>
2026-03-02 21:56:55 +0530

    Implement a basic login system in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,39 @@
 import java.util.Scanner;
 
-public class Hello {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);    System.out.println("Enter a num:" + "cm^2}
+class LoginSystem {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String correctUsername = "admin";
+        String correctPassword = "1234";
+
+        int attempts = 3;
+
+        while (attempts > 0) {
+
+            System.out.print("Enter Username: ");
+            String username = sc.next();
+
+            System.out.print("Enter Password: ");
+            String password = sc.next();
+
+            if (username.equals(correctUsername) && password.equals(correctPassword)) {
+                System.out.println("Login Successful ✅");
+                break;
+            } else {
+                attempts--;
+                System.out.println("Wrong credentials! Attempts left: " + attempts);
+            }
+        }
+
+        if (attempts == 0) {
+            System.out.println("Account Locked ❌");
+        }
+
+        sc.close();
+    }
 }
- 
-
-
     
+

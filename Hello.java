4ed669ab08f29d7db9bf8cd41c32e24bf93670de
S H Mahammed Saleeth <selith514@gmail.com>
2026-03-22 20:25:49 +0530

    Refactor Hello.java to remove duplicate classes

--- Hello.java
+++ Hello.java
@@ -250,4 +250,70 @@ class DiceGame {
 }
 
 
+import java.util.Scanner;
 
+class ATMSystem {
+
+    static Scanner sc = new Scanner(System.in);
+    static int pin = 1234;
+    static double balance = 1000;
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter your PIN: ");
+        int enteredPin = sc.nextInt();
+
+        if (enteredPin != pin) {
+            System.out.println("Wrong PIN ❌");
+            return;
+        }
+
+        int choice;
+
+        do {
+            System.out.println("\n--- ATM MENU ---");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Balance: ₹" + balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter deposit amount: ");
+                    double dep = sc.nextDouble();
+                    balance += dep;
+                    System.out.println("Deposit successful!");
+                    break;
+
+                case 3:
+                    System.out.print("Enter withdraw amount: ");
+                    double wd = sc.nextDouble();
+
+                    if (wd <= balance) {
+                        balance -= wd;
+                        System.out.println("Please collect cash 💵");
+                    } else {
+                        System.out.println("Insufficient balance ❌");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Thank you!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}

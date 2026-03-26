35170c89256e975d2afe90623a14e1e44732401d
S H Mahammed Saleeth <selith514@gmail.com>
2026-03-26 22:52:39 +0530

    Number pattern. Java

--- java.java
+++ java.java
@@ -1530,7 +1530,29 @@ class DiceGame {
 }
 
 
+import java.util.Scanner;
 
+class NumberPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
 
 
 

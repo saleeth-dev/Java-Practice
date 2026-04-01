c69ed3422de8d2c3d94eb82dff6f981020211b7f
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-01 22:24:11 +0530

    Refactor Hello.java to remove duplicates
    Removed duplicate classes and consolidated code for better organization.

--- Hello.java
+++ Hello.java
@@ -434,3 +434,38 @@ class ATMSystem {
         sc.close();
     }
 }
+
+import java.util.Scanner;
+import java.util.Random;
+
+class GuessGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        int guess;
+
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("Too High ⬆️");
+            } 
+            else if (guess < number) {
+                System.out.println("Too Low ⬇️");
+            } 
+            else {
+                System.out.println("Correct! 🎉");
+            }
+
+        } while (guess != number);
+
+        sc.close();
+    }
+}

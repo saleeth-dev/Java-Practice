97314a58f7317ff1cee1e2427ce40b9f6955948b
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-11 21:58:47 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- java.java
+++ java.java
@@ -1648,7 +1648,27 @@ class SimpleInterest {
     }
 }
 
+import java.util.Scanner;
 
+class AddNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
 
 
 

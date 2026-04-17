adccecb32b38fdad3253fa319a8da7f941c9fc56
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-17 21:54:58 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- java.java
+++ java.java
@@ -1670,7 +1670,30 @@ class AddNumbers {
     }
 }
 
+import java.util.Scanner;
 
+class SimpleInterest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        double p = sc.nextDouble();
+
+        System.out.print("Enter Rate (%): ");
+        double r = sc.nextDouble();
+
+        System.out.print("Enter Time (years): ");
+        double t = sc.nextDouble();
+
+        double si = (p * r * t) / 100;
+
+        System.out.println("Simple Interest = ₹" + si);
+
+        sc.close();
+    }
+}
 
 
 

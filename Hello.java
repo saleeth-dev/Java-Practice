3b5554f4b7cffe805e54abcf3432f960c7789187
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-18 21:23:15 +0530

    Refactor Hello.java with multiple classes

--- Hello.java
+++ Hello.java
@@ -491,3 +491,25 @@ class EvenOdd {
         sc.close();
     }
 }
+
+import java.util.Scanner;
+
+class AddNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}

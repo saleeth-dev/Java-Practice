d182057dca58b142c655c34f2b7ab33dd2f457c1
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-20 21:50:43 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1695,7 +1695,47 @@ class SimpleInterest {
     }
 }
 
+import java.util.Scanner;
 
+class StudentGradeManager {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        String[] names = new String[n];
+        double[] marks = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nStudent " + (i + 1));
+            System.out.print("Enter name: ");
+            names[i] = sc.next();
+
+            System.out.print("Enter marks: ");
+            marks[i] = sc.nextDouble();
+        }
+
+        System.out.println("\n===== STUDENT RESULTS =====");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(names[i] + " - " + marks[i] + " - Grade: ");
+
+            if (marks[i] >= 75)
+                System.out.println("A");
+            else if (marks[i] >= 60)
+                System.out.println("B");
+            else if (marks[i] >= 50)
+                System.out.println("C");
+            else
+                System.out.println("Fail");
+        }
+
+        sc.close();
+    }
+}
 
 
 

ddb4ba1206653f4c6c8a12e702c912afd6a19c3b
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-20 21:48:45 +0530

    Refactor Hello.java to consolidate classes

--- Hello.java
+++ Hello.java
@@ -513,3 +513,35 @@ class AddNumbers {
         sc.close();
     }
 }
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = sc.nextLine();
+
+        System.out.print("what is the price for each?: ");
+        price = sc.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = sc.nextInt();
+
+        total = price * quantity;
+        System.out.println(total  + " " + currency);
+
+
+        sc.close();
+
+
+
+    }
+}

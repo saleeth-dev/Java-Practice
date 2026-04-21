bdacf76478843915b4f0745963ef07c19eb9210c
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-21 21:35:16 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- java.java
+++ java.java
@@ -1737,6 +1737,29 @@ class StudentGradeManager {
     }
 }
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double radius;
+        double circonference;
+        double area;
+        double volume;
+        System.out.print("Enter the radius of the circle: ");
+        radius = sc.nextDouble();
+
+        circonference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+        System.out.printf("The circumference of the circle is: %.2fcm\n", circonference);
+        System.out.printf("The area of the circle is: %.2fcm²\n", area);
+        System.out.printf("The volume of the sphere is: %.2fcm³\n", volume);
+
+        sc.close();
+    }
+}
 
 
 

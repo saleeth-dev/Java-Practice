a648b60a160ef0d9f7c706d112263972fd0c8eab
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-22 10:59:25 +0530

    Refactor Hello.java for improved structure

--- Hello.java
+++ Hello.java
@@ -545,3 +545,23 @@ public class Main {
 
     }
 }
+
+class Animal {
+  String type = "Animal";
+}
+
+class Dog extends Animal {
+  String type = "Dog";
+
+  public void printType() {
+    System.out.println(super.type); // Access parent attribute
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Dog myDog = new Dog();
+    myDog.printType();
+  }
+}
+

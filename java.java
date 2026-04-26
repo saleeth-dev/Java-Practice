da1bb157062b11f98a4cf259716476d206c1b469
S H Mahammed Saleeth <selith514@gmail.com>
2026-04-26 22:44:15 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- java.java
+++ java.java
@@ -1820,7 +1820,58 @@ public class Main {
     }
 
 
+import java.util.Scanner;
 
+class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Simple Calculator ===");
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Choose Operation:");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+
+        switch(choice) {
+            case 1:
+                System.out.println("Result = " + (a + b));
+                break;
+
+            case 2:
+                System.out.println("Result = " + (a - b));
+                break;
+
+            case 3:
+                System.out.println("Result = " + (a * b));
+                break;
+
+            case 4:
+                if (b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+        }
+
+        sc.close();
+    }
+}
 
 
 

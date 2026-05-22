bd9bc6ba723b65f91912883209aac04f9c711856
S H Mahammed Saleeth <selith514@gmail.com>
2026-05-22 22:11:08 +0530

    Update java.java

--- java.java
+++ java.java
@@ -2177,7 +2177,30 @@ class FoodOrderSystem {
     }
 }
 
+import { useState, useEffect } from "react";
 
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Count changed");
+  }, [count]);
+
+  return (
+    <div>
+
+      <h1>{count}</h1>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
 
 
 

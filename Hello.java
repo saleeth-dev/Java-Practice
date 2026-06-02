c9792941ac9dc517325d59d078d3bb3e88ded999
S H Mahammed Saleeth <selith514@gmail.com>
2026-06-02 22:41:50 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- Hello.java
+++ Hello.java
@@ -942,3 +942,31 @@ class EmployeeSalary {
         sc.close();
     }
 }
+
+import java.util.Random;
+import java.util.Scanner;
+
+class PasswordGenerator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*";
+
+        System.out.print("Enter password length: ");
+        int length = sc.nextInt();
+
+        String password = "";
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(chars.length());
+            password += chars.charAt(index);
+        }
+
+        System.out.println("\nGenerated Password: " + password);
+
+        sc.close();
+    }
+}

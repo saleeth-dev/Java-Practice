cb741aa36ac25bf5068d85a2ac16d9225d50c3db
S H Mahammed Saleeth <selith514@gmail.com>
2026-06-22 22:24:27 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- Hello.java
+++ Hello.java
@@ -1095,3 +1095,38 @@ class SupermarketBill {
         sc.close();
     }
 }
+
+import java.util.Scanner;
+
+public class ATMPinSystem {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int correctPin = 1234;
+        int attempts = 3;
+
+        while (attempts > 0) {
+
+            System.out.print("Enter ATM PIN: ");
+            int pin = sc.nextInt();
+
+            if (pin == correctPin) {
+                System.out.println("Login Successful ");
+                System.out.println("Welcome to ATM!");
+                break;
+            } else {
+                attempts--;
+                System.out.println("Wrong PIN ");
+                System.out.println("Attempts Left: " + attempts);
+            }
+        }
+
+        if (attempts == 0) {
+            System.out.println("Account Locked ");
+        }
+
+        sc.close();
+    }
+}
